bfd11ef8fb4f2dc50f6ff6dacd2037a19800e379
Farid Huseynov <F.H189@hotmail.com>
2019-03-21 22:01:06 +0400

    Added required folders Added interface and class for registration and navigation of the viewmodels.

--- PubFinder/App.xaml.cs
+++ PubFinder/App.xaml.cs
@@ -13,5 +13,10 @@ namespace PubFinder
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var locator = new ViewModelLocator();
+
+        }
     }
 }
---
+++ PubFinder/Services/NavigationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace PubFinder.Services
+{
+    class NavigationService : INavigationService
+    {
+        private Dictionary<Type, ViewModelBase> viewModels = new Dictionary<Type, ViewModelBase>();
+
+        public void Navigate<T>()
+        {
+            Messenger.Default.Send(viewModels[typeof(T)]);
+        }
+
+        public void Navigate(Type type)
+        {
+            Messenger.Default.Send(viewModels[type]);
+        }
+
+        public void Register<T>(ViewModelBase viewModel)
+        {
+            viewModels.Add(typeof(T),viewModel);
+        }
+    }
+}
