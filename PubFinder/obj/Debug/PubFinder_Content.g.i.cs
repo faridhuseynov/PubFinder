749f4d7f7f65f464bc60e7621c1577fc9d26c12b
Farid Huseynov <F.H189@hotmail.com>
2019-03-31 16:46:26 +0400

    MaterialDesign set. Has to be continued and tested

--- PubFinder/obj/Debug/PubFinder_Content.g.i.cs
+++ PubFinder/obj/Debug/PubFinder_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/starticon.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/beerdaha_logo.png")]
 
 
