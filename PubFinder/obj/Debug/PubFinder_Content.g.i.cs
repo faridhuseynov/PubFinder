32ee2caad87c210fdd768311ce785e0682995b7a
Farid Huseynov <faridhuseynov.eng@gmail.com>
2019-09-21 13:37:44 +0400

    Fonts added Menus and sets updated

--- PubFinder/obj/Debug/PubFinder_Content.g.i.cs
+++ PubFinder/obj/Debug/PubFinder_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sources/start.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sources/gettyimages.jpg")]
 
 
